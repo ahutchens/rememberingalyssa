2d8fc5feae306f4abb698e2c560fddbba00659df
Austinb <austin.m.babcock@gmail.com>
2026-05-23 18:47:06 -0500

    feat: add date based turnoff for shirts tab

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,64 +1,75 @@
 var clicked = false;
 
-$(document).ready(function(){
-    $(".hamburger").click(function(){
+$(document).ready(function () {
+    const cutoff = Date.parse("2026-05-27T17:00:00Z");
+    const shirtsExpired = Date.now() >= cutoff;
+
+    if (shirtsExpired) {
+        $('.subdropdown a[href="shirt.html"]').hide();
+    }
+
+    if (shirtsExpired && window.location.pathname.toLowerCase().endsWith("/shirt.html")) {
+        $(".shirtcontent").html("<h1>Shirt sales are now closed.</h1>");
+    }
+
+    $(".hamburger").click(function () {
         $(".hamburger").toggleClass("open");
         $(".main").toggleClass("mainopen");
     });
-    $(".giving1").click(function(){
+    $(".giving1").click(function () {
         $(".selection1").toggleClass('hide');
         $(".selection2, .selection3, .selection4").addClass('hide');
         $("#triangle-up1").toggleClass('hidet');
         $("#triangle-up2, #triangle-up3, #triangle-up4").addClass('hidet');
-        if (!clicked){
-              clicked = true;
-              setTimeout(function(){ clicked = false; }, 1000);
-              }else{
-                return false;
-            };
-        });
-    $(".giving2").click(function(){
+        if (!clicked) {
+            clicked = true;
+            setTimeout(function () { clicked = false; }, 1000);
+        } else {
+            return false;
+        };
+    });
+    $(".giving2").click(function () {
         $(".selection2").toggleClass('hide');
         $(".selection1, .selection3, .selection4").addClass('hide');
         $("#triangle-up2").toggleClass('hidet');
         $("#triangle-up1, #triangle-up3, #triangle-up4").addClass('hidet');
-        if (!clicked){
-              clicked = true;
-              setTimeout(function(){ clicked = false; }, 1000);
-              }else{
-                return false;
-              };
+        if (!clicked) {
+            clicked = true;
+            setTimeout(function () { clicked = false; }, 1000);
+        } else {
+            return false;
+        };
     });
-    $(".giving3").click(function(){
+    $(".giving3").click(function () {
         $(".selection3").toggleClass('hide');
         $(".selection1, .selection2, .selection4").addClass('hide');
         $("#triangle-up3").toggleClass('hidet');
         $("#triangle-up1, #triangle-up2, #triangle-up4").addClass('hidet');
-        if (!clicked){
-              clicked = true;
-              setTimeout(function(){ clicked = false; }, 1000);
-              }else{
-                return false;
-              };
+        if (!clicked) {
+            clicked = true;
+            setTimeout(function () { clicked = false; }, 1000);
+        } else {
+            return false;
+        };
     });
-    $(".giving4").click(function(){
+    $(".giving4").click(function () {
         $(".selection4").toggleClass('hide');
         $(".selection1, .selection2, .selection3").addClass('hide');
         $("#triangle-up4").toggleClass('hidet');
         $("#triangle-up1, #triangle-up2, #triangle-up3").addClass('hidet');
-        if (!clicked){
-              clicked = true;
-              setTimeout(function(){ clicked = false; }, 1000);
-              }else{
-                return false;
-              };
+        if (!clicked) {
+            clicked = true;
+            setTimeout(function () { clicked = false; }, 1000);
+        } else {
+            return false;
+        };
     });
-    $("#dropdown1").click(function(){
-      $(".showelement").toggleClass("opensub");
-      $(".showelement2").removeClass("opensub");
+    $("#dropdown1").click(function () {
+        $(".showelement").toggleClass("opensub");
+        $(".showelement2").removeClass("opensub");
     });
-    $("#dropdown2").click(function(){
-      $(".showelement2").toggleClass("opensub");
-      $(".showelement").removeClass("opensub");
+    $("#dropdown2").click(function () {
+        $(".showelement2").toggleClass("opensub");
+        $(".showelement").removeClass("opensub");
     });
 });
